ae6ed0c00c28d85e0a89bbe777df49145811d57b
chohogeol <asn1101@naver.com>
2025-08-05 15:04:08 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwG0vwnK4Ei8K8vsV1f1YGs3LKkE1T99ZTE4a9H0-HNQt5Dqphy5GECoqeShiiQe4cE/exec"
+  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwlT7Ut4DY7Lfsb7NDXMQVDB72Rl9JHieVoPDNqsE8zNShKinbBYiwLxtzQfxmKYfVr/exec"
 };
