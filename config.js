d9226f14df769ca5370bfc3d73a4762ac32ea63b
chohogeol <asn1101@naver.com>
2025-08-05 15:13:13 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,2 @@
-// config.js
-module.exports = {
-  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwlT7Ut4DY7Lfsb7NDXMQVDB72Rl9JHieVoPDNqsE8zNShKinbBYiwLxtzQfxmKYfVr/exec"
-};
+// config.js (ESM 방식)
+export const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwlT7Ut4DY7Lfsb7NDXMQVDB72Rl9JHieVoPDNqsE8zNShKinbBYiwLxtzQfxmKYfVr/exec";
